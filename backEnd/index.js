5ba1a53ff07eada0eda631b1955efcc52cfc53a6
Anna Sedlar <annasedlar@gmail.com>
2017-06-04 18:17:32 -0400

    changed port to 3333 and added npm start to scripts in package.json

--- backEnd/index.js
+++ backEnd/index.js
@@ -20,7 +20,7 @@ app.post('/signup', function (req, res) {
 });
 
 
-app.listen(3000, function () {
-  console.log('Example app listening on port 3000!');
+app.listen(3333, function () {
+  console.log('Example app listening on port 3333!');
 });
 
