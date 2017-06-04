d70e6f69deb5ba8189a1c10d3ae77afc3466c58d
Anna Sedlar <annasedlar@gmail.com>
2017-06-04 17:37:15 -0400

    init

---
+++ backEnd/index.js
@@ -0,0 +1,26 @@
+var express = require('express');
+var bodyParser = require('body-parser');
+var app = express();
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
+
+app.get('/', function (req, res) {
+  res.send('Hello World!');
+});
+
+app.use(express.static('views'));
+
+app.get('/signup', function (req, res) {
+  res.sendfile(signup.html, {root: './'});
+});
+
+app.post('/signup', function (req, res) {
+  // save user details to your database.
+  res.send('Signed Up!');
+});
+
+
+app.listen(3000, function () {
+  console.log('Example app listening on port 3000!');
+});
+
