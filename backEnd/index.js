978e5395895feb64e049f475368e1906e0a25c5f
Anna Sedlar <annasedlar@gmail.com>
2017-06-04 20:28:51 -0400

    getting data from req.body

--- backEnd/index.js
+++ backEnd/index.js
@@ -4,22 +4,28 @@ var app = express();
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
 // app.use(express.static('views'));
-app.use(express.static('views'));
-
-// app.use(express.static(path.join(__dirname, 'views')));
+var path = require('path');
+app.use(express.static(path.join(__dirname, 'views')));
 
 app.get('/', function (req, res) {
-  res.send('Hello World!');
+  // res.send('Hello World!');
 });
 
-app.get('/signup.html', function (req, res) {
-  res.send("functiona");
+app.get('/signup', function (req, res) {
+  // res.send("functiona");
 });
+
+// app.get('/signup', function (req, res) {
+//   res.send("functiona");
+// });
 
 
 app.post('/signup', function (req, res) {
   // save user details to your database.
-  res.send('Signed Up!');
+  var details = req.body;
+  var firstname = req.body.firstname;
+  console.log(details);
+  res.send("Hi " + firstname + "!");
 });
 
 
